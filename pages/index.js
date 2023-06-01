3372230a741f673c602714748ecb8ee058f1a80c
AgentJakeHolcroft <jake@helloagent.co.uk>
2023-06-01 15:42:14 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,15 @@ export default function Home() {
         <meta name="description" content="Simply one paragraph of Lorem Ipsum." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+
+        <Head>
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content={"https://lipsum.one"} />
+      <meta property="og:title" content="Lipsum.One | Simply one paragraph of Lorem Ipsum. | Lipsum generator" />
+      <meta property="og:description" content="Simply one paragraph of Lorem Ipsum." />
+
       </Head>
       <main
   className={rubik.className + " w-[100%] h-[100vh] overflow-hidden flex justify-center items-center"}>
