eb6326a0a5d55c7458eef41424fbe8ee2f5aa64d
AgentJakeHolcroft <jake@helloagent.co.uk>
2023-06-01 15:43:04 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-
-        <Head>
-
       <meta property="og:type" content="website"/>
       <meta property="og:url" content={"https://lipsum.one"} />
       <meta property="og:title" content="Lipsum.One | Simply one paragraph of Lorem Ipsum. | Lipsum generator" />
